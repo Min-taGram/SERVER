fa24f34392ac0eaf89167f911f74a91aac54b57d
gandasipayung <gandasipayung14@gmail.com>
2020-01-16 18:36:44 +0700

    router user done

---
+++ Routes/userRouter.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+
+router.post('/register', UserController.registerUser)
+router.post('/login', UserController.loginUser)
+
+
+
+module.exports = router 
---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = process.env.PORT || 3000
+const router = require('./Routes')
+
+const mongoose = require('./configs/mongoose')
+mongoose()
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('OKE ')
+})
+
+app.listen(PORT, ()=> {
+  console.log('This App run on port : ', PORT);
+})
